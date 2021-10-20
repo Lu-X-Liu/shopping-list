197d76c2ecc809997636f86965ae22e1b1f2f135
Lu-X-Liu <91901553+Lu-X-Liu@users.noreply.github.com>
2021-10-20 14:07:09 -0500

    toggle bottom tab

--- main.js
+++ main.js
@@ -3,9 +3,16 @@
 const bottomTabIcon = document.querySelector('.add-new-category svg');
 const newCategoryBtnWrapper = document.querySelector('.new-category-btn-wrapper');
 
-bottomTabIcon.addEventListener('click', openBottomTab);
+bottomTabIcon.addEventListener('click', toggleBottomTab);
 
-function openBottomTab() {
-    bottomTabIcon.style.transform = 'rotate(90deg) translateX(-5px)';
-    newCategoryBtnWrapper.style.display = 'flex';
+function toggleBottomTab() {
+    const contentDisplay = newCategoryBtnWrapper.style;
+    if (contentDisplay.display === 'none' || contentDisplay.display === '') {
+        bottomTabIcon.style.transform = 'rotate(90deg) translateX(-5px)';
+        contentDisplay.display = 'block';
+    } else {
+        bottomTabIcon.style.transform = 'rotate(-90deg) translateX(5px)';
+        contentDisplay.display = 'none';
+    }     
 }
+
